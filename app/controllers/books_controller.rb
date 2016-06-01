38f70dd13d2b52bfd711951816a91ba476d8c743
Ito Satoshi <satoshi-ito@outlook.jp>
2016-06-01 16:10:05 +0900

    課題3,4修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
 
   # GET /books/new
   def new
-    @book = Book.new
+    @book = Book.new(price:0, cd:true)
   end
 
   # GET /books/1/edit
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to new_book_path(@book), notice: "「#{@book.title}」の登録完了" }
+        format.html { redirect_to new_book_path, notice: "「#{@book.title}」の登録完了" }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
